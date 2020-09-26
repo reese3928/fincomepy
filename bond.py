a8db2790824a84f6fb2baa1974caece8f3b2fc69
Xu Ren <xuren2120@gmail.com>
2020-09-26 00:28:19 -0400

    updated Bond class

--- bond.py
+++ bond.py
@@ -17,18 +17,21 @@ class FixedIncome(object):
         return regular_values*100
 
 class Bond(FixedIncome):
-    def __init__(self, settlement, maturity, coupon_perc, frequency, basis=1):
+    def __init__(self, settlement, maturity, coupon_perc, price_perc, frequency, basis=1):
+        ## TO DO: split clean price into 32nd
         self._settlement = settlement
         self._maturity = maturity
+        self._coupon_perc = coupon_perc
+        self._clean_price_perc = price_perc
         self._frequency = frequency
         self._basis = basis
         self._coupon_interval = 12 / frequency
-        self._periods = math.floor((self.diff_month(self._settlement, self._maturity))/self._coupon_interval)
-        self._coupon_dates = [self._maturity - relativedelta(months=self._coupon_interval)*i for i in range(self._periods + 1)]
-        self._couppcd = self._maturity - relativedelta(months=self._coupon_interval)*(self._periods + 1)
+        self._nperiod = math.floor((self.diff_month(self._settlement, self._maturity))/self._coupon_interval)
+        self._coupon_dates = [self._maturity - relativedelta(months=self._coupon_interval)*i for i in range(self._nperiod + 1)]
+        self._couppcd = self._maturity - relativedelta(months=self._coupon_interval)*(self._nperiod + 1)
         self._coupncd = self._coupon_dates[-1]
-        self._coupon_perc = coupon_perc
         self._accrint = self.accrint(self._couppcd, self._coupncd, self._settlement, self._coupon_perc, 100, self._frequency, self._basis)
+        self._dirty_price_perc = self._clean_price_perc + self._accrint
 
     @staticmethod
     def accrint(issue, first_interest, settlement, rate, par=100, frequency=2, basis=1):
@@ -47,21 +50,24 @@ class Bond(FixedIncome):
         return (date2.year - date1.year) * 12 + date2.month - date1.month
 
 
-bond_test = Bond(date(2010,10,10), date(2016,1,7), 4.75, 1, 1)
+bond_test = Bond(settlement=date(2020,7,15), maturity=date(2030,5,15), coupon_perc=0.625, 
+                 price_perc=(100+0.5/32), frequency=2, basis=1)
 bond_test._coupon_interval
-bond_test._periods
-bond_test._coupon_dates
-bond_test._couppcd
-bond_test._coupncd
-
-bond_test = Bond(date(2007,6,21), date(2012,5,31), 4.75, 2, 1)
-bond_test._coupon_interval
-bond_test._periods
-bond_test._coupon_dates
 bond_test._couppcd
 bond_test._coupncd
 bond_test._accrint
-Bond.accrint(bond_test._couppcd, bond_test._coupncd, bond_test._settlement, 4.75, par=100, frequency=2, basis=1)
+bond_test._dirty_price_perc
+
+Bond.accrint(issue=bond_test._couppcd, first_interest=bond_test._coupncd, settlement=bond_test._settlement, 
+             rate=0.625, par=100, frequency=2, basis=1)
 
 
+
+
+bond_test = Bond(date(2010,10,10), date(2016,1,7), 4.75, 1, 1)
+bond_test._coupon_interval
+bond_test._nperiod
+bond_test._coupon_dates
+bond_test._couppcd
+bond_test._coupncd
 
