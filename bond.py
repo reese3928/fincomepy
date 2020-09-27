e897b4c8bb7698144e2c93433817f9d683ef2d35
Xu Ren <xuren2120@gmail.com>
2020-09-27 11:34:01 -0400

    added convexity

--- bond.py
+++ bond.py
@@ -1,7 +1,6 @@
-from datetime import datetime, timedelta, date
+from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
 import numpy as np
-import pandas as pd
 import math
 from scipy.optimize import root
 
@@ -35,6 +34,10 @@ class Bond(FixedIncome):
         self._coupon_dates = [self._maturity - relativedelta(months=self._coupon_interval)*i for i in range(self._nperiod)]
         self._couppcd = self._maturity - relativedelta(months=self._coupon_interval)*(self._nperiod)
         self._coupncd = self._coupon_dates[-1]
+        if self._maturity==self.last_day_in_month(self._maturity):
+            self._coupon_dates = [self.last_day_in_month(item) for item in self._coupon_dates]
+            self._couppcd = self.last_day_in_month(self._couppcd)
+            self._coupncd = self.last_day_in_month(self._coupncd)
         self._accrint = self.accrint(self._couppcd, self._coupncd, self._settlement, self._coupon_perc, 100, self._frequency, self._basis)
         self._dirty_price_perc = self._clean_price_perc + self._accrint
 
@@ -54,6 +57,7 @@ class Bond(FixedIncome):
         dirty_price_regular = FixedIncome.perc_to_regular(self._dirty_price_perc)
         sol = root(lambda x: FixedIncome.perc_to_regular(self.price(x)) - dirty_price_regular, [0.01] )
         yield_perc = sol.x[0]
+        self._yield_regular = FixedIncome.perc_to_regular(yield_perc)
         return yield_perc
     
     def get_mac_duration(self):
@@ -77,6 +81,12 @@ class Bond(FixedIncome):
     def get_DV01(self):
         self._DV01 = self._mod_duration * FixedIncome.perc_to_regular(self._dirty_price_perc)
         return self._DV01
+    
+    def get_convexity(self):
+        self.CF_PV_times_p_2 = self.CF_PV * self._periods * self._periods
+        all = (self.CF_PV_times_p + self.CF_PV_times_p_2) / FixedIncome.perc_to_regular(self._dirty_price_perc)
+        self._convexity = all.sum() / (4 * (1 + self._yield_regular / self._frequency) ** 2)
+        return self._convexity
 
     @staticmethod
     def accrint(issue, first_interest, settlement, rate, par=100, frequency=2, basis=1):
@@ -91,6 +101,11 @@ class Bond(FixedIncome):
     @staticmethod
     def diff_month(date1, date2):
         return (date2.year - date1.year) * 12 + date2.month - date1.month
+    
+    @staticmethod
+    def last_day_in_month(original_date):
+        next_month = original_date.replace(day=28) + timedelta(days=4)
+        return next_month - timedelta(days=next_month.day)
 
 
 bond_test = Bond(settlement=date(2020,7,15), maturity=date(2030,5,15), coupon_perc=0.625, 
@@ -107,8 +122,21 @@ bond_test.get_mac_duration()
 bond_test = Bond(settlement=date(2020,7,15), maturity=date(2025,6,30), coupon_perc=0.25, 
                  price_perc=(99+26/32), frequency=2, basis=1)
 bond_test.get_yield()
-bond_test.get_mac_duration()
-bond_test.get_mod_duration()
+print("accrued_interest: {}".format(bond_test._accrint))
+print("Mac duration: {}".format(bond_test.get_mac_duration()))
+print("Modified duration: {}".format(bond_test.get_mod_duration()))
+print("DV01: {}".format(bond_test.get_DV01()))
+print("Convexity: {}".format(bond_test.get_convexity()))
+
+print("----------------------")
+bond_test = Bond(settlement=date(2020,7,15), maturity=date(2022,6,30), coupon_perc=1/8, 
+                 price_perc=(99+30/32), frequency=2, basis=1)
+bond_test.get_yield()
+print("accrued_interest: {}".format(bond_test._accrint))
+print("Mac duration: {}".format(bond_test.get_mac_duration()))
+print("Modified duration: {}".format(bond_test.get_mod_duration()))
+print("DV01: {}".format(bond_test.get_DV01()))
+print("Convexity: {}".format(bond_test.get_convexity()))
 
 Bond.accrint(issue=bond_test._couppcd, first_interest=bond_test._coupncd, settlement=bond_test._settlement, 
              rate=0.625, par=100, frequency=2, basis=1)
@@ -122,4 +150,5 @@ bond_test._nperiod
 bond_test._coupon_dates
 bond_test._couppcd
 bond_test._coupncd
+
 
