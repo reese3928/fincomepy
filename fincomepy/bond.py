dbb28e1fa2de611cf728ec40b7d918fcad988901
Xu Ren <xuren2120@gmail.com>
2020-09-30 22:57:17 -0400

    updated

--- fincomepy/bond.py
+++ fincomepy/bond.py
@@ -8,55 +8,77 @@ sys.path.append(".")  ## TO DO: check if this is can be added to __init__.py
 from fixedincome import FixedIncome
 
 ## TO DO: 
-# 1. change yield_regular -> yield
+# 1. change self._perc_dict["yield"] -> yield
 # 2. make regular number as a dict, perc as another dict
 # 3. add convexity adjustment (approximate bond price change using DV01 and convexity)
 
 class Bond(FixedIncome):
     def __init__(self, settlement, maturity, coupon_perc, price_perc, frequency, basis=1):
         ## TO DO: split clean price into 32nd
+        super().__init__()
         self._settlement = settlement
         self._maturity = maturity
-        self._coupon_perc = coupon_perc
-        self._clean_price_perc = price_perc
+        self._coupon_interval = 12 / frequency
+        self._nperiod = math.ceil((Bond.diff_month(self._settlement, self._maturity))/self._coupon_interval)
+        self._perc_dict["coupon"] = coupon_perc
+        self._perc_dict["clean_price"] = price_perc
         self._frequency = frequency
         self._basis = basis
-        self._coupon_interval = 12 / frequency
-        self._nperiod = math.ceil((self.diff_month(self._settlement, self._maturity))/self._coupon_interval)
-        self._coupon_dates = [self._maturity - relativedelta(months=self._coupon_interval)*i for i in range(self._nperiod)]
-        self._couppcd = self._maturity - relativedelta(months=self._coupon_interval)*(self._nperiod)
-        self._coupncd = self._coupon_dates[-1]
-        if self._maturity==self.last_day_in_month(self._maturity):
-            self._coupon_dates = [self.last_day_in_month(item) for item in self._coupon_dates]
-            self._couppcd = self.last_day_in_month(self._couppcd)
-            self._coupncd = self.last_day_in_month(self._coupncd)
-        self._accrint = self.accrint(self._couppcd, self._coupncd, self._settlement, self._coupon_perc, 100, self._frequency, self._basis)
-        self._dirty_price_perc = self._clean_price_perc + self._accrint
+        self._couppcd = Bond.couppcd(settlement, maturity, frequency, basis)
+        self._coupncd = Bond.coupncd(settlement, maturity, frequency, basis)
+        #self._coupon_dates = [self.last_day_in_month(item) for item in self._coupon_dates]
+        self._perc_dict["accrint"] = self.accrint(self._couppcd, self._coupncd, self._settlement, self._perc_dict["coupon"], 100, self._frequency, self._basis)
+        self._perc_dict["dirty_price"] = self._perc_dict["clean_price"] + self._perc_dict["accrint"]
+        self.update_dict()
     
-    ## TO DO: make a getter for couppcd, coupncd
+    @staticmethod
+    def couppcd(settlement, maturity, frequency, basis):
+        coupon_interval = 12 / frequency
+        nperiod = math.ceil((Bond.diff_month(settlement, maturity))/coupon_interval)
+        pcd = maturity - relativedelta(months=coupon_interval) * nperiod
+        if maturity==Bond.last_day_in_month(maturity):
+            return Bond.last_day_in_month(pcd)
+        return pcd
+    
+    @staticmethod
+    def coupncd(settlement, maturity, frequency, basis):
+        coupon_interval = 12 / frequency
+        nperiod = math.ceil((Bond.diff_month(settlement, maturity))/coupon_interval)
+        ncd = maturity - relativedelta(months=coupon_interval) * (nperiod - 1)
+        if maturity==Bond.last_day_in_month(maturity):
+            return Bond.last_day_in_month(ncd)
+        return ncd
+
+
+
+    # coupon_dates = [maturity - relativedelta(months=coupon_interval)*i for i in range(nperiod)]  ## TO DO: add coupon_dates calculation
+
 
     def price(self, yield_perc): ## to do change function name to dirty price
         first_period = (self._coupncd - self._settlement).days / (self._coupncd - self._couppcd).days
         self._periods = np.array([first_period + i for i in range(self._nperiod)])
-        CF_perc = np.array([self._coupon_perc / self._frequency] * self._nperiod)
+        CF_perc = np.array([self._perc_dict["coupon"] / self._frequency] * self._nperiod)
         CF_perc[-1] += 100  ## change this to par
-        self.CF_regular = FixedIncome.perc_to_regular(CF_perc)
-        yield_regular = FixedIncome.perc_to_regular(yield_perc)
+        #self._perc_dict["CF"] = CF_perc
+        #self._perc_dict["yield"] = yield_perc
+        #self.update_dict()
+        CF_regular = CF_perc*0.01
+        yield_regular = yield_perc * 0.01
         self.DF = 1 / (1 + yield_regular / self._frequency) ** self._periods
-        self.CF_PV = self.CF_regular * self.DF
+        self.CF_PV = CF_regular * self.DF
         CF_PV_total = sum(self.CF_PV)
-        return FixedIncome.regular_to_perc(CF_PV_total)
+        return CF_PV_total*100  ## TO DO: change this
     
     def get_yield(self):
-        dirty_price_regular = FixedIncome.perc_to_regular(self._dirty_price_perc)
-        sol = root(lambda x: FixedIncome.perc_to_regular(self.price(x)) - dirty_price_regular, [0.01] )
+        sol = root(lambda x: self.price(x)/100 - self._reg_dict["dirty_price"], [0.01] )
         yield_perc = sol.x[0]
-        self._yield_regular = FixedIncome.perc_to_regular(yield_perc)
+        self._perc_dict["yield"] = yield_perc
+        self.update_dict()
         return yield_perc
     
     def get_mac_duration(self):
         self.CF_PV_times_p = self.CF_PV * self._periods
-        return self.CF_PV_times_p.sum() / FixedIncome.perc_to_regular(self._dirty_price_perc) / self._frequency
+        return self.CF_PV_times_p.sum() / self._reg_dict["dirty_price"] / self._frequency
 
     def get_mod_duration(self):
         original_yield_perc = self.get_yield()
@@ -65,21 +87,21 @@ class Bond(FixedIncome):
         yield_down_perc = original_yield_perc - 0.01
         dirty_price_up_perc = self.price(yield_up_perc)
         dirty_price_down_perc = self.price(yield_down_perc)
-        price_change_up_perc = dirty_price_up_perc - self._dirty_price_perc
-        price_change_down_perc = dirty_price_down_perc - self._dirty_price_perc
-        relative_change_up = price_change_up_perc / self._dirty_price_perc
-        relative_change_down = price_change_down_perc / self._dirty_price_perc
-        self._mod_duration = (abs(relative_change_up) + abs(relative_change_down)) / 2 / FixedIncome.perc_to_regular(0.01)
+        price_change_up_perc = dirty_price_up_perc - self._perc_dict["dirty_price"]
+        price_change_down_perc = dirty_price_down_perc - self._perc_dict["dirty_price"]
+        relative_change_up = price_change_up_perc / self._perc_dict["dirty_price"]
+        relative_change_down = price_change_down_perc / self._perc_dict["dirty_price"]
+        self._mod_duration = (abs(relative_change_up) + abs(relative_change_down)) / 2 / (0.01*0.01) ## TO DO: change this
         return self._mod_duration
     
     def get_DV01(self):
-        self._DV01 = self._mod_duration * FixedIncome.perc_to_regular(self._dirty_price_perc)
+        self._DV01 = self._mod_duration * self._reg_dict["dirty_price"]
         return self._DV01
     
     def get_convexity(self):
         self.CF_PV_times_p_2 = self.CF_PV * self._periods * self._periods
-        all = (self.CF_PV_times_p + self.CF_PV_times_p_2) / FixedIncome.perc_to_regular(self._dirty_price_perc)
-        self._convexity = all.sum() / (4 * (1 + self._yield_regular / self._frequency) ** 2)
+        all = (self.CF_PV_times_p + self.CF_PV_times_p_2) / self._reg_dict["dirty_price"]
+        self._convexity = all.sum() / (4 * (1 + self._reg_dict["yield"] / self._frequency) ** 2)
         return self._convexity
 
     @staticmethod
@@ -88,9 +110,9 @@ class Bond(FixedIncome):
         ## TO DO: check if first_interest > issue
         total_days = (first_interest - issue).days
         accrued_days = (settlement - issue).days
-        rate_regular = FixedIncome.perc_to_regular(rate)
+        rate_regular = rate/100
         accrued_interest_perc = (rate_regular/frequency) * (accrued_days / total_days)
-        return FixedIncome.regular_to_perc(accrued_interest_perc)
+        return accrued_interest_perc*100
 
     @staticmethod
     def diff_month(date1, date2):
@@ -102,36 +124,5 @@ class Bond(FixedIncome):
         return next_month - timedelta(days=next_month.day)
 
 
-
-
-bond_test = Bond(settlement=date(2020,7,15), maturity=date(2025,6,30), coupon_perc=0.25, 
-                 price_perc=(99+26/32), frequency=2, basis=1)
-bond_test.get_yield()
-print("accrued_interest: {}".format(bond_test._accrint))
-print("Mac duration: {}".format(bond_test.get_mac_duration()))
-print("Modified duration: {}".format(bond_test.get_mod_duration()))
-print("DV01: {}".format(bond_test.get_DV01()))
-print("Convexity: {}".format(bond_test.get_convexity()))
-
-print("----------------------")
-bond_test = Bond(settlement=date(2020,7,15), maturity=date(2022,6,30), coupon_perc=1/8, 
-                 price_perc=(99+30/32), frequency=2, basis=1)
-bond_test.get_yield()
-print("accrued_interest: {}".format(bond_test._accrint))
-print("Mac duration: {}".format(bond_test.get_mac_duration()))
-print("Modified duration: {}".format(bond_test.get_mod_duration()))
-print("DV01: {}".format(bond_test.get_DV01()))
-print("Convexity: {}".format(bond_test.get_convexity()))
-print("----------------------")
-
-Bond.accrint(issue=bond_test._couppcd, first_interest=bond_test._coupncd, settlement=bond_test._settlement, 
-             rate=0.625, par=100, frequency=2, basis=1)
-
-bond_test = Bond(date(2010,10,10), date(2016,1,7), 4.75, 1, 1)
-bond_test._coupon_interval
-bond_test._nperiod
-bond_test._coupon_dates
-bond_test._couppcd
-bond_test._coupncd
 
 
--- tests/test_bond.py
+++ tests/test_bond.py
@@ -13,11 +13,11 @@ class Test(unittest.TestCase):
     def test_return_values(self):
         bond_test = Bond(settlement=date(2020,7,15), maturity=date(2030,5,15), coupon_perc=0.625, 
                  price_perc=(100+0.5/32), frequency=2, basis=1)
-        self.assertTrue(abs(bond_test._coupon_interval - 6.0) < 0.00001)
+        #self.assertTrue(abs(bond_test._coupon_interval - 6.0) < 0.00001)
         self.assertEqual(bond_test._couppcd, date(2020, 5, 15))
-        self.assertEqual(bond_test._couppcd, date(2020, 11, 15))
-        self.assertTrue(abs(bond_test._accrint - 0.1036) < 0.0001)
-        self.assertTrue(abs(bond_test._dirty_price_perc - 100.1192) < 0.0001)
+        self.assertEqual(bond_test._coupncd, date(2020, 11, 15))
+        self.assertTrue(abs(bond_test._perc_dict["accrint"] - 0.1036) < 0.0001)
+        self.assertTrue(abs(bond_test._perc_dict["dirty_price"] - 100.1192) < 0.0001)
         self.assertTrue(abs(bond_test.price(0.62334818) - 100.1192) < 0.0001)
         self.assertTrue(abs(bond_test.get_yield() - 0.6233) < 0.0001)
         self.assertTrue(abs(bond_test.get_mac_duration() - 9.5437) < 0.0001)
@@ -25,7 +25,7 @@ class Test(unittest.TestCase):
         bond_test = Bond(settlement=date(2020,7,15), maturity=date(2025,6,30), coupon_perc=0.25, 
                  price_perc=(99+26/32), frequency=2, basis=1)
         self.assertTrue(abs(bond_test.get_yield() - 0.2881) < 0.0001)
-        self.assertTrue(abs(bond_test._accrint - 0.0102 < 0.0001))
+        self.assertTrue(abs(bond_test._perc_dict["accrint"] - 0.0102 < 0.0001))
         self.assertTrue(abs(bond_test.get_mac_duration() - 4.9312 < 0.0001))
         self.assertTrue(abs(bond_test.get_mod_duration() - 4.9241 < 0.0001))
         self.assertTrue(abs(bond_test.get_DV01() - 4.9154 < 0.0001))
@@ -34,26 +34,22 @@ class Test(unittest.TestCase):
         bond_test = Bond(settlement=date(2020,7,15), maturity=date(2022,6,30), coupon_perc=1/8, 
                  price_perc=(99+30/32), frequency=2, basis=1)
         self.assertTrue(abs(bond_test.get_yield() - 0.1569 < 0.0001))
-        self.assertTrue(abs(bond_test._accrint - 0.0051 < 0.0001))
+        self.assertTrue(abs(bond_test._perc_dict["accrint"] - 0.0051 < 0.0001))
         self.assertTrue(abs(bond_test.get_mac_duration() - 1.9574 < 0.0001))
         self.assertTrue(abs(bond_test.get_mod_duration() - 1.9558 < 0.0001))
         self.assertTrue(abs(bond_test.get_DV01() - 1.9547 < 0.0001))
         self.assertTrue(abs(bond_test.get_convexity() - 4.8053 < 0.0001))
 
-        Bond.accrint(issue=bond_test._couppcd, first_interest=bond_test._coupncd, settlement=bond_test._settlement, 
-             rate=0.625, par=100, frequency=2, basis=1)
+        # Bond.accrint(issue=bond_test._couppcd, first_interest=bond_test._coupncd, settlement=bond_test._settlement, 
+        #     rate=0.625, par=100, frequency=2, basis=1)
 
-bond_test = Bond(date(2010,10,10), date(2016,1,7), 4.75, 1, 1)
-bond_test._coupon_interval
-bond_test._nperiod
-bond_test._coupon_dates
-bond_test._couppcd
-bond_test._coupncd
+        # bond_test = Bond(date(2010,10,10), date(2016,1,7), 4.75, 1, 1)
+        # bond_test._coupon_interval
+        # bond_test._nperiod
+        # bond_test._coupon_dates
+        # bond_test._couppcd
+        # bond_test._coupncd
 
-
-
-        
-    
     #def test_zspread_property(self):
         
 
