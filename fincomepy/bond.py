5f8e07a241ddbb06e06842b8898a3b12a4c3c49b
Xu Ren <xuren2120@gmail.com>
2020-10-03 10:06:39 -0400

    updated

--- fincomepy/bond.py
+++ fincomepy/bond.py
@@ -8,29 +8,30 @@ sys.path.append(".")  ## TO DO: check if this is can be added to __init__.py
 from fixedincome import FixedIncome
 
 ## TO DO: 
-# 1. change self._perc_dict["yield"] -> yield
-# 2. make regular number as a dict, perc as another dict
 # 3. add convexity adjustment (approximate bond price change using DV01 and convexity)
 
 class Bond(FixedIncome):
-    def __init__(self, settlement, maturity, coupon_perc, price_perc, frequency, basis=1):
+    def __init__(self, settlement, maturity, coupon_perc, price_perc, frequency, basis=1, redemption=100, yld=None):
         ## TO DO: split clean price into 32nd
         super().__init__()
         self._settlement = settlement
         self._maturity = maturity
-        self._coupon_interval = 12 / frequency  ## TO DO: remove this line
-        self._nperiod = math.ceil((Bond.diff_month(self._settlement, self._maturity))/self._coupon_interval)  ## TO DO: remove this line
         self._perc_dict["coupon"] = coupon_perc
         self._perc_dict["clean_price"] = price_perc
         self._frequency = frequency
         self._basis = basis
+        self._redemption = redemption
         self._couppcd = Bond.couppcd(settlement, maturity, frequency, basis)
         self._coupncd = Bond.coupncd(settlement, maturity, frequency, basis)
-        #self._coupon_dates = [self.last_day_in_month(item) for item in self._coupon_dates]
         self._perc_dict["accrint"] = Bond.accrint(issue=self._couppcd, first_interest=self._coupncd, settlement=self._settlement,
                                                   rate=self._perc_dict["coupon"], par=1, frequency=self._frequency, basis=self._basis)
         self._perc_dict["dirty_price"] = self._perc_dict["clean_price"] + self._perc_dict["accrint"]
         self.update_dict()
+        self._yld = yld   ## TO DO: check if yld can be put into perc_dict
+        self._mac_duration = None
+        self._mod_duration = None
+        self._DV01 = None
+        self._convexity = None
     
     @staticmethod
     def couppcd(settlement, maturity, frequency, basis):
@@ -60,7 +61,7 @@ class Bond(FixedIncome):
         return accrued_interest * par
 
     @staticmethod
-    def price2(settlement, maturity, rate, yld, redemption, frequency, basis):
+    def dirty_price(settlement, maturity, rate, yld, redemption, frequency, basis):
         pcd = Bond.couppcd(settlement, maturity, frequency, basis)
         ncd = Bond.coupncd(settlement, maturity, frequency, basis)
         first_period = (ncd - settlement).days / (ncd - pcd).days
@@ -82,53 +83,69 @@ class Bond(FixedIncome):
         ncd = Bond.coupncd(settlement, maturity, frequency, basis)
         accrued_interest = Bond.accrint(issue=pcd, first_interest=ncd, settlement=settlement, rate=rate, par=1, frequency=frequency, basis=basis)
         dirty_price_target = accrued_interest + pr
-        sol = root(lambda x: Bond.price2(settlement, maturity, rate, x, redemption, frequency, basis) - dirty_price_target, [0.01] )
+        sol = root(lambda x: Bond.dirty_price(settlement, maturity, rate, x, redemption, frequency, basis) - dirty_price_target, [0.01] )
         yld = sol.x[0]
         assert yld >= 0 and yld <= 100
         return yld
 
-    def price(self, yield_perc): ## to do change function name to dirty price
+    def intermediate_values(self):
+        coupon_interval = 12 / self._frequency  
+        nperiod = math.ceil((Bond.diff_month(self._settlement, self._maturity))/coupon_interval) 
         first_period = (self._coupncd - self._settlement).days / (self._coupncd - self._couppcd).days
-        self._periods = np.array([first_period + i for i in range(self._nperiod)])
-        CF_perc = np.array([self._perc_dict["coupon"] / self._frequency] * self._nperiod)
-        CF_perc[-1] += 100  ## change this to par
-        #self._perc_dict["CF"] = CF_perc
-        #self._perc_dict["yield"] = yield_perc
-        #self.update_dict()
+        periods = np.array([first_period + i for i in range(nperiod)])
+        CF_perc = np.array([self._perc_dict["coupon"] / self._frequency] * nperiod)
+        CF_perc[-1] += self._redemption
         CF_regular = CF_perc*0.01
-        yield_regular = yield_perc * 0.01
-        self.DF = 1 / (1 + yield_regular / self._frequency) ** self._periods
-        self.CF_PV = CF_regular * self.DF
-        CF_PV_total = sum(self.CF_PV)
-        return CF_PV_total*100  ## TO DO: change this
+        if self._yld is None:
+            self._yld = self.yld(self._settlement, self._maturity, self._perc_dict["coupon"], self._perc_dict["clean_price"],
+                                 self._redemption, self._frequency, self._basis)
+        yield_regular = self._yld * 0.01
+        DF = 1 / (1 + yield_regular / self._frequency) ** periods
+        return (periods, CF_regular, DF)
     
-    def get_mac_duration(self):
-        self.CF_PV_times_p = self.CF_PV * self._periods
-        return self.CF_PV_times_p.sum() / self._reg_dict["dirty_price"] / self._frequency
+    def mac_duration(self):
+        periods, CF_regular, DF = self.intermediate_values()
+        CF_PV = CF_regular * DF
+        CF_PV_times_p = CF_PV * periods
+        self._mac_duration = CF_PV_times_p.sum() / self._reg_dict["dirty_price"] / self._frequency
+        return self._mac_duration
 
-    def get_mod_duration(self):
-        original_yield_perc = Bond.yld(settlement=self._settlement, maturity=self._maturity, 
-            rate=self._perc_dict["coupon"], pr=self._perc_dict["clean_price"], redemption=100, frequency=2, basis=1)
-        ## TO DO: make 0.01 as an argument
-        yield_up_perc = original_yield_perc + 0.01
-        yield_down_perc = original_yield_perc - 0.01
-        dirty_price_up_perc = self.price(yield_up_perc)
-        dirty_price_down_perc = self.price(yield_down_perc)
+    def mod_duration(self, yld_change_perc=0.01):
+        if self._yld is None:
+            original_yield_perc = self.yld(self._settlement, self._maturity, self._perc_dict["coupon"], self._perc_dict["clean_price"],
+                                           self._redemption, self._frequency, self._basis)
+        else:
+            original_yield_perc = self._yld
+        yield_up_perc = original_yield_perc + yld_change_perc
+        yield_down_perc = original_yield_perc - yld_change_perc
+        dirty_price_up_perc = self.dirty_price(self._settlement, self._maturity, self._perc_dict["coupon"], yield_up_perc,
+                                               self._redemption, self._frequency, self._basis)  
+        dirty_price_down_perc = self.dirty_price(self._settlement, self._maturity, self._perc_dict["coupon"], yield_down_perc,
+                                               self._redemption, self._frequency, self._basis) 
         price_change_up_perc = dirty_price_up_perc - self._perc_dict["dirty_price"]
         price_change_down_perc = dirty_price_down_perc - self._perc_dict["dirty_price"]
         relative_change_up = price_change_up_perc / self._perc_dict["dirty_price"]
         relative_change_down = price_change_down_perc / self._perc_dict["dirty_price"]
-        self._mod_duration = (abs(relative_change_up) + abs(relative_change_down)) / 2 / (0.01*0.01) ## TO DO: change this
+        self._mod_duration = (abs(relative_change_up) + abs(relative_change_down)) / 2 / (yld_change_perc * 0.01) 
         return self._mod_duration
     
-    def get_DV01(self):
+    def DV01(self):
+        if self._mod_duration is None:
+            self.mod_duration()
         self._DV01 = self._mod_duration * self._reg_dict["dirty_price"]
         return self._DV01
     
-    def get_convexity(self):
-        self.CF_PV_times_p_2 = self.CF_PV * self._periods * self._periods
-        all = (self.CF_PV_times_p + self.CF_PV_times_p_2) / self._reg_dict["dirty_price"]
-        self._convexity = all.sum() / (4 * (1 + self._reg_dict["yield"] / self._frequency) ** 2)
+    def convexity(self):
+        periods, CF_regular, DF = self.intermediate_values()
+        CF_PV = CF_regular * DF
+        CF_PV_times_p = CF_PV * periods
+        CF_PV_times_p_2 = CF_PV * periods * periods
+        all = (CF_PV_times_p + CF_PV_times_p_2) / self._reg_dict["dirty_price"]
+        if self._yld is None:
+            self._yld = self.yld(self._settlement, self._maturity, self._perc_dict["coupon"], self._perc_dict["clean_price"],
+                                 self._redemption, self._frequency, self._basis)
+        yield_regular = self._yld * 0.01
+        self._convexity = all.sum() / (4 * (1 + yield_regular / self._frequency) ** 2)
         return self._convexity
 
     @staticmethod
--- tests/test_bond.py
+++ tests/test_bond.py
@@ -13,32 +13,50 @@ class Test(unittest.TestCase):
     def test_return_values(self):
         bond_test = Bond(settlement=date(2020,7,15), maturity=date(2030,5,15), coupon_perc=0.625, 
                  price_perc=(100+0.5/32), frequency=2, basis=1)
-        #self.assertTrue(abs(bond_test._coupon_interval - 6.0) < 0.00001)
         self.assertEqual(bond_test._couppcd, date(2020, 5, 15))
         self.assertEqual(bond_test._coupncd, date(2020, 11, 15))
         self.assertTrue(abs(bond_test._perc_dict["accrint"] - 0.1036) < 0.0001)
         self.assertTrue(abs(bond_test._perc_dict["dirty_price"] - 100.1192) < 0.0001)
-        self.assertTrue(abs(bond_test.price(0.62334818) - 100.1192) < 0.0001)
-        #self.assertTrue(abs(bond_test.get_yield() - 0.6233) < 0.0001)
-        self.assertTrue(abs(bond_test.get_mac_duration() - 9.5437) < 0.0001)
+        dirty_price = Bond.dirty_price(settlement=date(2020,7,15), maturity=date(2030,5,15),
+                      rate=0.625, yld=0.62334818, redemption=100, frequency=2, basis=1)
+        self.assertTrue(abs(dirty_price - 100.1192) < 0.0001)
+        yld = Bond.yld(settlement=date(2020,7,15), maturity=date(2030,5,15), rate=0.625,
+                    pr=(100+0.5/32), redemption=100, frequency=2, basis=1)
+        self.assertTrue(abs(yld - 0.6233) < 0.0001)
+        yld = Bond.yld(settlement=date(2020,7,15), maturity=date(2030,5,15), rate=0.625,
+                    pr=(100+0.5/32), redemption=105, frequency=2, basis=1)
+        self.assertTrue(abs(yld - 1.1060) < 0.0001)
+        self.assertTrue(bond_test._mac_duration is None)
+        self.assertTrue(bond_test._yld is None)
+        self.assertTrue(abs(bond_test.mac_duration() - 9.5437) < 0.0001)
+        self.assertFalse(bond_test._mac_duration is None)
+        self.assertFalse(bond_test._yld is None)
 
         bond_test = Bond(settlement=date(2020,7,15), maturity=date(2025,6,30), coupon_perc=0.25, 
                  price_perc=(99+26/32), frequency=2, basis=1)
-        #self.assertTrue(abs(bond_test.get_yield() - 0.2881) < 0.0001)
+        yld = Bond.yld(settlement=date(2020,7,15), maturity=date(2025,6,30), rate=0.25, 
+                 pr=(99+26/32), redemption=100, frequency=2, basis=1)
+        self.assertTrue(abs(yld - 0.2881) < 0.0001)
         self.assertTrue(abs(bond_test._perc_dict["accrint"] - 0.0102 < 0.0001))
-        self.assertTrue(abs(bond_test.get_mac_duration() - 4.9312 < 0.0001))
-        self.assertTrue(abs(bond_test.get_mod_duration() - 4.9241 < 0.0001))
-        self.assertTrue(abs(bond_test.get_DV01() - 4.9154 < 0.0001))
-        self.assertTrue(abs(bond_test.get_convexity() - 26.8053 < 0.0001))
+        self.assertTrue(abs(bond_test.mac_duration() - 4.9312 < 0.0001))
+        self.assertTrue(bond_test._mod_duration is None)
+        self.assertTrue(abs(bond_test.mod_duration() - 4.9241 < 0.0001))
+        self.assertFalse(bond_test._mod_duration is None)
+        self.assertTrue(bond_test._DV01 is None)
+        self.assertTrue(abs(bond_test.DV01() - 4.9154 < 0.0001))
+        self.assertFalse(bond_test._DV01 is None)
+        self.assertTrue(bond_test._convexity is None)
+        self.assertTrue(abs(bond_test.convexity() - 26.8053 < 0.0001))
+        self.assertFalse(bond_test._convexity is None)
 
         bond_test = Bond(settlement=date(2020,7,15), maturity=date(2022,6,30), coupon_perc=1/8, 
                  price_perc=(99+30/32), frequency=2, basis=1)
-        self.assertTrue(abs(bond_test.get_yield() - 0.1569 < 0.0001))
         self.assertTrue(abs(bond_test._perc_dict["accrint"] - 0.0051 < 0.0001))
-        self.assertTrue(abs(bond_test.get_mac_duration() - 1.9574 < 0.0001))
-        self.assertTrue(abs(bond_test.get_mod_duration() - 1.9558 < 0.0001))
-        self.assertTrue(abs(bond_test.get_DV01() - 1.9547 < 0.0001))
-        self.assertTrue(abs(bond_test.get_convexity() - 4.8053 < 0.0001))
+        self.assertTrue(abs(bond_test.mac_duration() - 1.9574 < 0.0001))
+        self.assertTrue(abs(bond_test.mod_duration() - 1.9558 < 0.0001))
+        self.assertTrue(abs(bond_test.DV01() - 1.9547 < 0.0001))
+        self.assertTrue(abs(bond_test.convexity() - 4.8053 < 0.0001))
+        self.assertTrue(abs(bond_test._yld - 0.1569 < 0.0001))
 
         # Bond.accrint(issue=bond_test._couppcd, first_interest=bond_test._coupncd, settlement=bond_test._settlement, 
         #     rate=0.625, par=100, frequency=2, basis=1)
@@ -58,15 +76,6 @@ class Test(unittest.TestCase):
         accrued_interest = Bond.accrint(issue=pcd, first_interest=ncd, settlement=date(2020,7,15),
                                         rate=0.625, par=1, frequency=2, basis=1)
         self.assertTrue(abs(accrued_interest - 0.1036) < 0.0001)
-        dirty_price = Bond.price2(settlement=date(2020,7,15), maturity=date(2030,5,15), rate=0.625,
-                                  yld=0.62334818, redemption=100, frequency=2, basis=1)
-        self.assertTrue(abs(dirty_price - 100.1192) < 0.0001)
-        yld = Bond.yld(settlement=date(2020,7,15), maturity=date(2030,5,15), rate=0.625,
-                                  pr=(100+0.5/32), redemption=100, frequency=2, basis=1)
-        self.assertTrue(abs(yld - 0.6233) < 0.0001)
-        yld = Bond.yld(settlement=date(2020,7,15), maturity=date(2030,5,15), rate=0.625,
-                                  pr=(100+0.5/32), redemption=105, frequency=2, basis=1)
-        self.assertTrue(abs(yld - 1.1060) < 0.0001)
     
     def test_invalid_input(self):
         with self.assertRaises(Exception):
