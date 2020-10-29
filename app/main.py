59de84e38628ddac0077199d65d347d393c69da8
Xu Ren <xuren2120@gmail.com>
2020-10-28 22:09:54 -0400

    included about page

--- app/main.py
+++ app/main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
