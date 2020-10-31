8122d8dab9309c39a42994f7bece8b1aeed70628
Xu Ren <xuren2120@gmail.com>
2020-10-30 22:44:23 -0400

    started bond calculation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, url_for, request 
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date, datetime
+from fincomepy import *
+## TO DO: change this fincomepy to local 
+## TO DO: check if different product can be put into separate python files
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,14 +13,27 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/about", methods=['GET', 'POST'])
+@app.route("/about")
 def about():
+    return render_template('about.html')
+
+@app.route("/analysis", methods=['GET', 'POST'])
+def analysis():
     if request.method == 'POST':
-        number1 = float(request.form['number1'])
-        number2 = float(request.form['number2'])
-        res = number1 * number2
-        return render_template('home.html', res=res)
-    return render_template('home.html')
+        # get input
+        settlement = datetime.strptime(request.form['settlement'], '%Y-%m-%d').date()
+        maturity = datetime.strptime(request.form['maturity'], '%Y-%m-%d').date()
+        coupon_perc = float(request.form['coupon_perc'])
+        price_perc = float(request.form['price_perc'])
+        frequency = int(request.form['frequency'])
+        basis = int(request.form['basis'])
+        # construct a bond object
+        bond_obj = Bond(settlement=settlement, maturity=maturity, coupon_perc=coupon_perc, 
+            price_perc=price_perc, frequency=frequency, basis=basis)
+        
+        res = bond_obj.mac_duration()
+        return render_template('analysis.html', res=res)
+    return render_template('analysis.html')
 
 
 
