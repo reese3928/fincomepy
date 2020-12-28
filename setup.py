f56569b2602e3aa5d518a20de7575d5a6e1cca76
Xu Ren <xuren2120@gmail.com>
2020-12-28 16:21:28 -0500

    fix package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = ['numpy', 'datetime', 'python-dateutil', 
+requirements = ['numpy>=1.18.5', 'datetime', 'python-dateutil', 
     'matplotlib']
 
 setup_requirements = ['pytest-runner']
